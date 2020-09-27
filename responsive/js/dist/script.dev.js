8ce0b4717e070b2b65942b48e6749abf1754ba5a
Rakhattt <67402354+Rakhattt@users.noreply.github.com>
2020-09-27 22:22:29 +0600

    responsive

---
+++ responsive/js/dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function ($) {
+  $(function () {
+    $('ul.discover__blogs').on('click', 'li:not(.discover__undertitle_active)', function () {
+      $(this).addClass('discover__undertitle_active').siblings().removeClass('discover__undertitle_active').closest('div.discover__wrapper').find('div.discover__blog').removeClass('discover__blog_active').eq($(this).index()).addClass('discover__blog_active');
+    });
+    var tabIndex = window.location.hash.replace('#discover', '8') - 1;
+    if (tabIndex != -1) $('ul.discover__blogs li').eq(tabIndex).click();
+  });
+})(jQuery);
